f6f6245618316f7b9db7656ea02d6ef5f0391924
vishalKushwaha977 <59596343+vishalKushwaha977@users.noreply.github.com>
2020-01-28 10:21:48 +0530

    commit

---
+++ DataBase/driver_tracker_drive_stud.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: driver_tracker
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `drive_stud`
+--
+
+DROP TABLE IF EXISTS `drive_stud`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `drive_stud` (
+  `DSID` int(11) NOT NULL AUTO_INCREMENT,
+  `DID` int(11) DEFAULT NULL,
+  `ST_ID` int(11) DEFAULT NULL,
+  `CREATION_DATE` timestamp NULL DEFAULT NULL,
+  `TPOID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`DSID`),
+  KEY `DID_key_idx` (`DID`),
+  KEY `TPOID_key_idx` (`TPOID`),
+  KEY `TPOID_FR_key_idx` (`TPOID`),
+  CONSTRAINT `DID_key` FOREIGN KEY (`DID`) REFERENCES `drive` (`DID`),
+  CONSTRAINT `TPOID_FR_key` FOREIGN KEY (`TPOID`) REFERENCES `tpo` (`TPOID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drive_stud`
+--
+
+LOCK TABLES `drive_stud` WRITE;
+/*!40000 ALTER TABLE `drive_stud` DISABLE KEYS */;
+/*!40000 ALTER TABLE `drive_stud` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-27 16:00:31
